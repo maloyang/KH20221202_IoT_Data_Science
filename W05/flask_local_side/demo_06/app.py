5d44cd0d57105da01c715d881614d63ffbe685d9
maloyang <maloyang@gmail.com>
2023-01-14 16:56:03 +0800

    update w05

--- W05/flask_local_side/demo_06/app.py
+++ W05/flask_local_side/demo_06/app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template, jsonify, request
 from firebase import firebase
+import time
+import random
 
 app = Flask(__name__)
 
@@ -44,8 +46,21 @@ def api_firebase_log2_get():
 @app.route("/api/firebase/log2", methods=['POST'])
 def api_firebase_log2_post():
     # do something
+    tag_name = '/tag_malo'
+    my_firebase = firebase.FirebaseApplication('https://fire-test-c46d1.firebaseio.com', None)
+    my_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    my_data = {'data': {'T': round(random.uniform(25, 30), 1), 
+                        'H': round(random.uniform(50, 90), 1)}, 'time': my_time}
+    my_firebase.put(tag_name+'/log2', my_time, my_data)
+
     result = {'result':'OK'}
     return jsonify(result)
+
+@app.route("/api/iot", methods=['GET'])
+def api_iot_get():
+    # access database
+    my_data = {'data':{'temp':28, 'humi':80, 'wind':1.5, 'co2':230}, 'result':'OK', 'log':''}
+    return jsonify(my_data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
